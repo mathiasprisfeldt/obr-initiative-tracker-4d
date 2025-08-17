590c5e580046766bedc35c9d06fe7520aa8f30e7
Mathias Prisfeldt <mathias.prisfeldt@vippsmobilepay.com>
2025-08-17 23:31:23 +0200

    Open tracker if player

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,5 +1,7 @@
 import OBR from "@owlbear-rodeo/sdk";
+import { OpenTracker } from "../tracker/Tracker";
 
 OBR.onReady(async () => {
   if ((await OBR.player.getRole()) === "GM") return;
+  OpenTracker();
 });

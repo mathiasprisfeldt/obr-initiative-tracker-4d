0800754b650e3fe8dba3fce81537ab206e70c180
Mathias Prisfeldt <mathias.prisfeldt@vippsmobilepay.com>
2025-11-20 22:28:36 +0100

    Add debug button to download metadata

--- src/admin/DebugPanel.tsx
+++ src/admin/DebugPanel.tsx
@@ -1,14 +1,11 @@
 import { Button, Stack } from "@mui/material";
 import { useTrackerStore } from "../store/tracker-store";
 import { usePortraitImagePickerStore } from "../character-portrait/portrait-image-picker-store";
+import OBR from "@owlbear-rodeo/sdk";
 
 export function DebugPanel() {
     const { state: trackerState } = useTrackerStore();
     const { state: portraitImagePickerState } = usePortraitImagePickerStore();
-
-    if (import.meta.env.PROD) {
-        return null;
-    }
 
     return (
         <Stack direction="row" spacing={2}>
@@ -21,6 +18,17 @@ export function DebugPanel() {
                 onClick={() => console.log(portraitImagePickerState)}
             >
                 Print portrait image picker state
+            </Button>
+            <Button
+                onClick={async () => {
+                    const metadata = await OBR.room.getMetadata();
+                    open(
+                        "data:text/json," + encodeURIComponent(JSON.stringify(metadata)),
+                        "_blank",
+                    );
+                }}
+            >
+                Download metadata
             </Button>
         </Stack>
     );
